b20b789dd578028e1df3f60eb255296a4e7d023d
NabievAzamat <azamat.nabiev21@yandex.ru>
2020-01-27 23:27:54 +0500

    'cycles'

---
+++ src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int i =0;
+//        while(i < 10) {
+//            if(i == 5)
+//                break;
+//            if(i % 2 == 0) {
+//                i++;
+//                continue;
+//            }
+//            System.out.println(i);
+//            i++;
+//        }
+//        LinkedList<String> names = new LinkedList<>();
+//        names.add("Ильшат");
+//        names.add("Виктор");
+//        names.add("Дмитрий");
+//        names.add("Артём");
+//        names.add("Мират");
+//        for (String item: names){
+//            System.out.println(item);
+//        }
+//        int[][] big_arr = new int[][]{
+//                {1,2,3,4},
+//                {76,21,79,32},
+//                {0,56,38,93}
+//        };
+//        for(int i = 0; i<big_arr.length;i++){
+//            for(int j = 0; j<big_arr[i].length;j++) {
+//                System.out.print(big_arr[i][j] + " ");
+//         }
+//            System.out.println(" ");
+//        }
+
+//        short[] arr = new short[] {5,7,1,21,4545,32,8};
+//        for(int i = 0; i<arr.length;i++){
+//            System.out.println("Значение элемента " + (i+1) + " = " + arr[i]);
+//        }
+
+//        int user_input;
+//        Scanner in = new Scanner(System.in);
+//        do{
+//            System.out.println("Введите число 21");
+//            user_input = in.nextInt();
+//        } while(user_input != 21);
+//        System.out.println("Вы молодец!");
+    }
+}
