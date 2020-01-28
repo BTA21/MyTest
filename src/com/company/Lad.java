fb6c09561d3f1706c280ee9cd7c9d479a424c992
NabievAzamat <azamat.nabiev21@yandex.ru>
2020-01-28 15:13:37 +0500

    OOP extends (still in commit process)

---
+++ src/com/company/Lad.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Lad extends Car {
+    private boolean isMPerformance = false;
+    Lad(boolean isMPerformance, String model, float weight,
+        String color, boolean isWork, short speed) {
+        super(model, weight, color, isWork);
+        this.isMPerformance = isMPerformance;
+        this.speed = speed;
+    }
+
+    void printAll() {
+        super.printAll();
+        String isMPerformance = this.isMPerformance?"является":"не является";
+        System.out.println("Машина " + isMPerformance + " М перформанс");
+    }
+}
+
