079bcf0b1d69e6970a0127ab5e508f73a6e196d4
NabievAzamat <azamat.nabiev21@yandex.ru>
2020-01-28 14:39:03 +0500

    OOP business

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Car lada = new Car((short)160);
+//        lada.speed = 160;
+        lada.setAll("kalina",1320.2f,"White",true);
+        lada.printAll();
+//        System.out.println(lada.speed);
+
+        Car chevrolet = new Car("camaro",1072.67f,"yellow",true);
+        chevrolet.speed = 200;
+//        chevrolet.setAll("camaro",1072.67f,"yellow",true);
+//        chevrolet.printAll();
+//        System.out.println(chevrolet.speed);
 
     }
+
 
 }
